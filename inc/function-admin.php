33bcc8ed09b8cddb2be274938dd8cab4e6bf35a2
Jason Rametta <rametta@outlook.com>
2016-06-17 19:24:35 -0400

    updated

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -7,6 +7,7 @@
 	========================
 	 Theme Admin Page
 	========================
+
 */
 
 add_action('admin_menu', 'rametta_add_admin_page');
@@ -18,7 +19,6 @@ function rametta_add_admin_page() {
 	// rametta admin submenu pages
 	add_submenu_page('rametta', 'Rametta Theme Options', 'Settings', 'manage_options', 'rametta', 'rametta_theme_create_page');
 	add_submenu_page('rametta', 'Rametta Style Options', 'Style', 'manage_options', 'rametta_style', 'rametta_theme_settings_page');
-
 	// activate custom settings
 	add_action('admin_init', 'rametta_custom_settings');
 }
@@ -35,6 +35,8 @@ function rametta_theme_settings_page() {
 
 function rametta_custom_settings() {
 	register_setting('rametta-settings-group', 'first_name');
+	register_setting('rametta-settings-group', 'last_name');
+
 	add_settings_section('rametta-sidebar-options', 'Sidebar Options', 'rametta_sidebar_options', 'rametta');
 	add_settings_field('sidebar-name', 'First Name', 'rametta_sidebar_name', 'rametta', 'rametta-sidebar-options');
 }
@@ -45,5 +47,7 @@ function rametta_sidebar_options() {
 
 function rametta_sidebar_name() {
 	$firstName = esc_attr(get_option('first_name'));
-	echo '<input type="text" name="first_name" value="'. $firstName .'" placeholder="First Name" />';
+	$lastName = esc_attr(get_option('last_name'));
+	echo '<input type="text" name="first_name" value="'. $firstName .'" placeholder="First Name" />
+	<input type="text" name="last_name" value="'. $lastName .'" placeholder="Last Name" />';
 }
